4701bb7fed130e89e429ff1c94385c54899ec1de
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2025-06-28 00:31:21 +0700

    install and configure git

--- users/dsyafaatul/home.nix
+++ users/dsyafaatul/home.nix
@@ -3,5 +3,14 @@
   home.username = "dsyafaatul";
   home.homeDirectory = "/home/dsyafaatul";
   
+  programs.git = {
+    enable = true;
+    userEmail = "dikisyafaatul@gmail.com";
+    userName = "D. Syafa'atul Anbiya";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+  
   home.stateVersion = "25.05";
 }
