8eb53c7dbf20475f69a74b83b3919452eaf4aa61
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2025-06-29 15:17:29 +0700

    dconf setting

--- users/dsyafaatul/home.nix
+++ users/dsyafaatul/home.nix
@@ -36,6 +36,30 @@
   home.shellAliases = {
     switch = "sudo nixos-rebuild switch --flake ~/nix/#HPPavilion14";
   };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+    "io/elementary/desktop/background" = {
+      dim-wallpaper-in-dark-style = true;
+    };
+    "io/elementary/dock" = {
+      icon-size = 32;
+      autohide-mode = "overlapping-window";
+    };
+    "io/elementary/desktop/wm/behavior/move-maximized-workspace" = {
+      move-maximized-workspace = true;
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>t";
+      command = "io.elementary.terminal";
+      name = "io.elementary.terminal";
+    };
+    "io/elementary/desktop/wingpanel/power" = {
+      show-percentage = true;
+    };
+  };
   
   home.stateVersion = "25.05";
 }
